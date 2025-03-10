7e1540fffd98b97ea2dd475eb559f77f6e2f1bc7
hrokr <alex.rook@gmail.com>
2025-03-10 18:14:54 -0400

    Add intial tests for main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,19 +3,19 @@ import sys
 import sqlite3
 import pytest
 
-from main import DB_PATH  # , main
+from main import DONOR_DB_PATH
 
 
 def test_database_exists():
     """Ensure the database file exists."""
-    assert os.path.exists(DB_PATH), (
+    assert os.path.exists(DONOR_DB_PATH), (
         "Database file does not exist. Try running donor_generator.py"
     )
 
 
 def test_donors_table_has_data():
     """Check that the donors table contains at least one record."""
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DONOR_DB_PATH)
     cursor = conn.cursor()
     cursor.execute("SELECT COUNT(*) FROM donors")
     count = cursor.fetchone()[0]
